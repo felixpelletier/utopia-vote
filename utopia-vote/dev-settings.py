48a21760c7e55138565bedf3640fd24117649bb8
Félix Pelletier <felixpelletier23@gmail.com>
2016-06-25 13:20:24 -0400

    Updated for heroku database

--- utopia-vote/dev-settings.py
+++ utopia-vote/dev-settings.py
@@ -2,3 +2,10 @@ from .settings import *
 
 SOCIAL_AUTH_FACEBOOK_KEY = '112118909214327'
 SOCIAL_AUTH_FACEBOOK_SECRET = '82addc72e75a94a0e267481600f0d3d6'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- utopia-vote/settings.py
+++ utopia-vote/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'utopia-vote.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(),
 }
 
 # Password validation
